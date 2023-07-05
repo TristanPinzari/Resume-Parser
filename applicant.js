abda7bb6f780ef44a1708a7c4771f6b858fbc726
Tristan <tristanpinzari@example.com>
2023-07-05 19:26:40 -0400

    AlmostDone

--- applicant.js
+++ applicant.js
@@ -54,14 +54,155 @@ document.querySelector("form").addEventListener("submit", function (e) {
     .then(function (snapshot) {
       uploadButton.textContent = "Uploaded";
       alertfunction("Upload Successful!");
-      // Get the download URL for the PDF file
+      // Convert PDF to text
       pdfFileRef.getDownloadURL()
         .then(function (url) {
-          // Display the download link
-          var downloadLink = document.getElementById("downloadLink");
-          downloadLink.href = url;
-          downloadLink.textContent = "Download Original PDF";
-          downloadLink.style.display = "block";
+          fetch(url)
+            .then(function (response) {
+              return response.arrayBuffer();
+            })
+            .then(function (arrayBuffer) {
+              var uint8Array = new Uint8Array(arrayBuffer);
+              return pdfjsLib.getDocument(uint8Array).promise;
+            })
+            .then(function (pdfDoc) {
+              var numPages = pdfDoc.numPages;
+              var textContent = "";
+              var promises = [];
+
+              for (var i = 1; i <= numPages; i++) {
+                promises.push(pdfDoc.getPage(i).then(function (page) {
+                  return page.getTextContent().then(function (content) {
+                    content.items.forEach(function (item) {
+                      textContent += item.str + " ";
+                    });
+                  });
+                }));
+              }
+
+              Promise.all(promises).then(function () {
+                // Upload the text file to Firebase Storage
+                var textFileRef = storageRef.child(file.name + ".txt");
+                return textFileRef.putString(textContent);
+              }).then(function () {
+                // Get the download URL for the text file
+                return textFileRef.getDownloadURL();
+              }).then(function (textFileUrl) {
+                // Display the link to the text file
+                var textLink = document.getElementById("textLink");
+                textLink.href = textFileUrl;
+                textLink.textContent = "View Text File";
+                textLink.style.display = "block";
+              }).catch(function (error) {
+                console.log(error);
+              });
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    })
+    .catch(function (error) {
+      uploadButton.textContent = "Upload";
+      alertfunction("Upload Unsuccessful");
+      console.log(error);
+    });
+});firebase.initializeApp(firebaseConfig);
+
+// File input change event listener
+document.getElementById("file-input").addEventListener("change", function (event) {
+  var selectedFile = event.target.files[0];
+  var chooseFileButton = document.getElementById("chooselabel");
+
+  if (selectedFile) {
+    chooseFileButton.textContent = selectedFile.name;
+    document.getElementById("uploadButton").textContent = "Upload";
+  } else {
+    chooseFileButton.textContent = "Choose File";
+    document.getElementById("uploadButton").textContent = "No File Selected";
+  }
+});
+
+// Form submission event listener
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent form submission
+
+  function alertfunction(message) {
+    alert(message);
+  }
+
+  var fileInput = document.getElementById("file-input");
+  var file = fileInput.files[0];
+  var uploadButton = document.getElementById("uploadButton");
+
+  if (!file) {
+    alertfunction("No File Selected");
+    return;
+  }
+
+  if (file.type !== "application/pdf") {
+    alertfunction("Invalid file type. Please select a PDF file.");
+    return;
+  }
+
+  uploadButton.textContent = "Uploading";
+
+  var storageRef = firebase.storage().ref();
+  var pdfFileRef = storageRef.child(file.name);
+
+  pdfFileRef.put(file)
+    .then(function (snapshot) {
+      uploadButton.textContent = "Uploaded";
+      alertfunction("Upload Successful!");
+      // Convert PDF to text
+      pdfFileRef.getDownloadURL()
+        .then(function (url) {
+          fetch(url)
+            .then(function (response) {
+              return response.arrayBuffer();
+            })
+            .then(function (arrayBuffer) {
+              var uint8Array = new Uint8Array(arrayBuffer);
+              return pdfjsLib.getDocument(uint8Array).promise;
+            })
+            .then(function (pdfDoc) {
+              var numPages = pdfDoc.numPages;
+              var textContent = "";
+              var promises = [];
+
+              for (var i = 1; i <= numPages; i++) {
+                promises.push(pdfDoc.getPage(i).then(function (page) {
+                  return page.getTextContent().then(function (content) {
+                    content.items.forEach(function (item) {
+                      textContent += item.str + " ";
+                    });
+                  });
+                }));
+              }
+
+              Promise.all(promises).then(function () {
+                // Upload the text file to Firebase Storage
+                var textFileRef = storageRef.child(file.name + ".txt");
+                return textFileRef.putString(textContent);
+              }).then(function () {
+                // Get the download URL for the text file
+                return textFileRef.getDownloadURL();
+              }).then(function (textFileUrl) {
+                // Display the link to the text file
+                var textLink = document.getElementById("textLink");
+                textLink.href = textFileUrl;
+                textLink.textContent = "View Text File";
+                textLink.style.display = "block";
+              }).catch(function (error) {
+                console.log(error);
+              });
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
         })
         .catch(function (error) {
           console.log(error);
--- recruiter.js
+++ recruiter.js
@@ -1,9 +1,9 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCBvhvxXNxpJLYdX4-N0rMUnsQe0J09dFQ",
-    authDomain: "resume-parser-488ed.firebaseapp.com",
-    projectId: "resume-parser-488ed",
-    storageBucket: "resume-parser-488ed.appspot.com",
-    appId: "1:967002194526:web:cc839f6f01f1dbbe2a6acf",
+  apiKey: "AIzaSyCBvhvxXNxpJLYdX4-N0rMUnsQe0J09dFQ",
+  authDomain: "resume-parser-488ed.firebaseapp.com",
+  projectId: "resume-parser-488ed",
+  storageBucket: "resume-parser-488ed.appspot.com",
+  appId: "1:967002194526:web:cc839f6f01f1dbbe2a6acf",
 };
 
 // Get references to the necessary elements
@@ -15,55 +15,55 @@ firebase.initializeApp(firebaseConfig);
 // Event listener for the display button
 searchbar.addEventListener('change', function() {
 
-    while (fileForm.firstChild) {
-        fileForm.removeChild(fileForm.firstChild);
-    }
-
-    if (searchbar.value === "") {
+  while (fileForm.firstChild) {
       fileForm.removeChild(fileForm.firstChild);
-    }
+  }
 
-    firebase
-    .storage()
-    .ref()
-    .listAll()
-    .then(function(result) {
-      result.items.forEach(function(fileRef) {
-        // Get the download URL for the file
-        fileRef
-          .getDownloadURL()
-          .then(function(url) {
-            // Fetch the file content from the URL
-            fetch(url, {
-              method: "GET"
-          })
-              .then(function(response) {
-                return response.text();
-              })
-              .then(function(fileContent) {
-                if (fileContent.toLowerCase().includes(searchbar.value)) {
-                  // Create a link element for the file
-                  const fileLink = document.createElement('a');
-                  fileLink.href = url;
-                  fileLink.textContent = fileRef.name;
-                  fileLink.target = '_blank';
+  if (searchbar.value === "") {
+    fileForm.removeChild(fileForm.firstChild);
+  }
 
-                  // Append the link to the wrapper form
-                  fileForm.appendChild(fileLink);
-                }
-              })
-              
-              .catch(function(error) {
-                console.log('Error fetching file:', error);
-              });
-          })
+  firebase
+  .storage()
+  .ref()
+  .listAll()
+  .then(function(result) {
+    result.items.forEach(function(fileRef) {
+      // Get the download URL for the file
+      fileRef
+        .getDownloadURL()
+        .then(function(url) {
+          // Fetch the file content from the URL
+          fetch(url, {
+            method: "GET"
+        })
+            .then(function(response) {
+              return response.text();
+            })
+            .then(function(fileContent) {
+              if (fileContent.toLowerCase().includes(searchbar.value)) {
+                // Create a link element for the file
+                const fileLink = document.createElement('a');
+                fileLink.href = url;
+                fileLink.textContent = fileRef.name;
+                fileLink.target = '_blank';
 
-          .catch(function(error) {
-            console.log('Error getting download URL:', error);
-          });
-      });
-    })
-    .catch(function(error) {
-      console.log('Error retrieving files from Firebase Storage:', error);
+                // Append the link to the wrapper form
+                fileForm.appendChild(fileLink);
+              }
+            })
+            
+            .catch(function(error) {
+              console.log('Error fetching file:', error);
+            });
+        })
+
+        .catch(function(error) {
+          console.log('Error getting download URL:', error);
+        });
     });
+  })
+  .catch(function(error) {
+    console.log('Error retrieving files from Firebase Storage:', error);
+  });
 });
