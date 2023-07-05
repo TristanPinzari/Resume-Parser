8610b5306d6fb8a5964311f36f77186b2afbf1bd
Tristan <tristanpinzari@example.com>
2023-07-05 18:37:57 -0400

    GotPDFReaderWorking

--- applicant.js
+++ applicant.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyCBvhvxXNxpJLYdX4-N0rMUnsQe0J09dFQ",
-    authDomain: "resume-parser-488ed.firebaseapp.com",
-    projectId: "resume-parser-488ed",
-    storageBucket: "resume-parser-488ed.appspot.com",
-    appId: "1:967002194526:web:cc839f6f01f1dbbe2a6acf",
+  apiKey: "AIzaSyCBvhvxXNxpJLYdX4-N0rMUnsQe0J09dFQ",
+  authDomain: "resume-parser-488ed.firebaseapp.com",
+  projectId: "resume-parser-488ed",
+  storageBucket: "resume-parser-488ed.appspot.com",
+  appId: "1:967002194526:web:cc839f6f01f1dbbe2a6acf",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -16,10 +16,10 @@ document.getElementById("file-input").addEventListener("change", function (event
 
   if (selectedFile) {
     chooseFileButton.textContent = selectedFile.name;
-    document.getElementById("uploadButton").textContent = "Upload"
+    document.getElementById("uploadButton").textContent = "Upload";
   } else {
     chooseFileButton.textContent = "Choose File";
-    document.getElementById("uploadButton").textContent = "No File Selected"
+    document.getElementById("uploadButton").textContent = "No File Selected";
   }
 });
 
@@ -27,34 +27,87 @@ document.getElementById("file-input").addEventListener("change", function (event
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form submission
 
-  function alertfunction(e) {
-    alert(e);
+  function alertfunction(message) {
+    alert(message);
   }
 
   var fileInput = document.getElementById("file-input");
   var file = fileInput.files[0];
-  var uploadbutton = document.getElementById("uploadButton");
+  var uploadButton = document.getElementById("uploadButton");
 
-if (file) {
-  uploadbutton.textContent = "Uploading"
-}
+  if (!file) {
+    alertfunction("No File Selected");
+    return;
+  }
 
-//  if (file.type !== "application/pdf") {
-//    alertfunction("Invalid file type. Please select a PDF file.");
-//    return;
-//  }
+  if (file.type !== "application/pdf") {
+    alertfunction("Invalid file type. Please select a PDF file.");
+    return;
+  }
 
-  var storageRef = firebase.storage().ref();
-  var fileRef = storageRef.child(file.name);
+  uploadButton.textContent = "Uploading";
 
-  fileRef
-    .put(file)
-    .then(function (snapshot) {
-      uploadbutton.textContent = "Uploaded"
-      alertfunction("Upload Successful!");
-    })
-    .catch(function (error) {
-      alertfunction("Upload Unsuccessful");
-      // Handle error during upload
-    });
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    var arrayBuffer = event.target.result;
+    var uint8Array = new Uint8Array(arrayBuffer);
+
+    pdfjsLib.getDocument(uint8Array)
+      .promise.then(function (pdfDoc) {
+        var numPages = pdfDoc.numPages;
+        var textContent = "";
+
+        function processPage(pageNum) {
+          return pdfDoc.getPage(pageNum).then(function (page) {
+            return page.getTextContent().then(function (content) {
+              content.items.forEach(function (item) {
+                textContent += item.str + " ";
+              });
+            });
+          });
+        }
+
+        var promises = [];
+        for (var i = 1; i <= numPages; i++) {
+          promises.push(processPage(i));
+        }
+
+        Promise.all(promises)
+          .then(function () {
+            // All pages processed, upload the text file to Firebase
+            var storageRef = firebase.storage().ref();
+            var textFileRef = storageRef.child(file.name + ".txt");
+
+            textFileRef.putString(textContent)
+              .then(function (snapshot) {
+                uploadButton.textContent = "Uploaded";
+                alertfunction("Upload Successful!");
+              })
+              .catch(function (error) {
+                uploadButton.textContent = "Upload";
+                alertfunction("Upload Unsuccessful");
+                // Handle error during upload
+              });
+          })
+          .catch(function (error) {
+            uploadButton.textContent = "Upload";
+            alertfunction("Error occurred while processing the PDF file");
+            console.log(error);
+          });
+      })
+      .catch(function (error) {
+        uploadButton.textContent = "Upload";
+        alertfunction("Error occurred while loading the PDF file");
+        console.log(error);
+      });
+  };
+
+  reader.onerror = function (event) {
+    uploadButton.textContent = "Upload";
+    alertfunction("Error occurred while reading the file");
+    console.log(event.target.error);
+  };
+
+  reader.readAsArrayBuffer(file);
 });
